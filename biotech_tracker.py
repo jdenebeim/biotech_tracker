13982d018aa13283390eeb9c27cc5230a70fd5b0
Jack Denebeim <jrdenebeim@gmail.com>
2025-07-29 15:49:39 -0400

    Create biotech_tracker.py

---
+++ biotech_tracker.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Jack Denebeim
+"""
+ 
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import smtplib
+from email.mime.text import MIMEText
+
+url = "https://www.biospace.com/biospace-layoff-tracker"
+ 
+response = requests.get(url)
+ 
+# Check if the request was successful
+if response.status_code == 200:
+    # Create a Beautiful Soup object
+    soup = BeautifulSoup(response.text, 'html.parser')
+else:
+    print(f"Failed to retrieve content. Status code: {response.status_code}")
+ 
+ 
+ 
+for child in (soup.find_all('b')):
+    for sib in child.next_siblings:
+        if(sib.name == 'i'):
+            mon = sib.text.split(sep = " ")[0]
+            if ("." in mon):
+                mon_num = datetime.datetime.strptime(mon[:-1], '%b').month
+            else:
+                mon_num = datetime.datetime.strptime(mon, '%B').month
+            day_num = sib.text.split(sep = " ")[1]
+            date = datetime.date(2025, int(mon_num), int(day_num))
+            email_message = date.today().strftime("%B %d, %Y")
+            if date == datetime.date.today():
+                email_message += f"\nThere was an update!\n{url}"
+            else:
+                email_message += "\nNo updates today."
+
+def send_email(subject, body, sender_email, app_password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = sender_email  # sending to yourself
+
+    with smtplib.SMTP('smtp.mail.me.com', 587) as server:
+        server.starttls()
+        server.login(sender_email, app_password)
+        server.send_message(msg)
+ 
+
+print(email_message)   
+
+send_email(
+    subject="Layoff_Tracker_Update!",
+    body=email_message,
+    sender_email=email,
+    app_password=password)
