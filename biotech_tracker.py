9d5af25479f80e027a6227f0a41521ce430edb7b
Jack Denebeim <jrdenebeim@gmail.com>
2025-07-30 19:11:24 -0400

    Update biotech_tracker.py

--- biotech_tracker.py
+++ biotech_tracker.py
@@ -24,22 +24,27 @@ if response.status_code == 200:
 else:
     print(f"Failed to retrieve content. Status code: {response.status_code}")
  
+email_message = datetime.date.today().strftime("%B %d, %Y")
+
+found_update = False
+
 for child in (soup.find_all('b')):
-    for sib in child.next_siblings:
-        if(sib.name == 'i'):
-            mon = sib.text.split(sep = " ")[0]
-            if ("." in mon):
-                mon_num = datetime.datetime.strptime(mon[:-1], '%b').month
-            else:
-                mon_num = datetime.datetime.strptime(mon, '%B').month
-            day_num = sib.text.split(sep = " ")[1]
-            date = datetime.date(2025, int(mon_num), int(day_num))
-            print(date)
-            email_message = date.today().strftime("%B %d, %Y")
-            if str(date) == str(datetime.date.today()):
-                email_message += f"\nThere was an update!\n{url}"
-            else:
-                email_message += "\nNo updates today."
+    if (not found_update):
+        for sib in child.next_siblings:
+            if(sib.name == 'i'):
+                mon = sib.text.split(sep = " ")[0]
+                if ("." in mon):
+                    mon_num = datetime.datetime.strptime(mon[:-1], '%b').month
+                else:
+                    mon_num = datetime.datetime.strptime(mon, '%B').month
+                day_num = sib.text.split(sep = " ")[1]
+                date = datetime.date(2025, int(mon_num), int(day_num))
+                if (date == datetime.date.today()):
+                    email_message += f"\nThere was an update!\n{url}"
+                    found_update = True
+
+if (not found_update):
+    email_message += "\nNo updates today."
 
 def send_email(subject, body, sender_email, app_password):
     msg = MIMEText(body)
@@ -51,9 +56,7 @@ def send_email(subject, body, sender_email, app_password):
         server.starttls()
         server.login(sender_email, app_password)
         server.send_message(msg)
- 
-print(date.today())
-print(date)
+
 print(email_message)   
 
 send_email(
